b6d2eae4b2235bc1896d07b2538436966cc24e17
Aaron Williams <axwabq423@gmail.com>
2025-09-26 23:24:03 -0400

    Pass Test Case 2: Course planning with if/elif/else and comparison operators

--- [username]_assignment_3.py
+++ [username]_assignment_3.py
@@ -15,4 +15,33 @@ print(stress_level)
 git add [username]_assignment_3.py
 git commit -m "Pass Test Case 1: Initial game setup with required variables"
 
+# Decision 1
+print("A) Light (12 credits)")
+print("b) Standard (15 credits)")
+print("c) Heavy (18 credits)")
+input = ("Choose your cource laod: ")
+
+if input == "A":
+  if current_gpa >= 3.5:
+    print("Light Load")
+  else:
+    stress_level += 12
+elif input == "B":
+  if current_gpa >= 3.0:
+    print("Standard")
+  else:
+    social_points - 10
+elif input == "C":
+  if current_gpa >= 2.5:
+    print("Heavy Load")
+  else:
+    study_hours += 20
+else:
+  print("Please choose another option")
+        
+  
+
+
+
+
 
